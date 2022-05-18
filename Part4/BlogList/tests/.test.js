d6664040296a7ae9a79b89ec8b9b032859a3201a
JanatB <janatbaig2@gmail.com>
2022-05-18 17:57:44 -0400

    Completed exercise 4.3 - 4.5

---
+++ Part4/BlogList/tests/.test.js
@@ -0,0 +1,126 @@
+const testHelper = require('../utils/test_helper')
+
+// Test Arrays
+const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+]
+const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+]
+
+// Tests
+describe('totalLikes', () => {
+
+    test('of an empty array', () => {
+        const array = []
+        const result = testHelper.totalLikes(array)
+        expect(result).toBe(0)
+    })
+
+
+    test('of list of length 1', () => {
+        const likesList = listWithOneBlog.map((blog) => blog.likes);
+        const result = testHelper.totalLikes(likesList)
+        expect(result).toBe(5)
+        
+    })
+
+    test('of a larger list', () => {
+        const likesList = blogs.map(blog => blog.likes)
+        const result = testHelper.totalLikes(likesList)
+        expect(result).toBe(36)
+    })
+
+})
+
+describe('mostLikes', () => {
+
+    test('of array length 1', () => {
+        const result = testHelper.favBlog(listWithOneBlog)
+        expect(result).toEqual(
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            }
+        )
+    })
+
+    test('of bigger array', () => {
+        const result = testHelper.favBlog(blogs)
+        console.log(result)
+        expect(result).toEqual(
+            {
+                _id: "5a422b3a1b54a676234d17f9",
+                title: "Canonical string reduction",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                likes: 12,
+                __v: 0
+              }
+        )
+    })
+
+    test('of empty array', () => {
+        const result = testHelper.favBlog([])
+        expect(result).toBe("no blogs")
+    })
+
+})
