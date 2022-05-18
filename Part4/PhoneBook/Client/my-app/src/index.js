6ea2f68ffe663f42ef966926206db5d77317ba8f
JanatB <janatbaig2@gmail.com>
2022-05-17 20:46:24 -0400

    deleted files

--- Part4/PhoneBook/Client/my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />,document.getElementById('root'));
-
-
-// Running the JSON server: npx json-server --watch my-app/db.json --port 8000
